a2584edabcd7a1e8e0553731ff4a5d52e15b69ed
nyszhangyong <nyszhangyong@foxmail.com>
2016-10-24 15:14:02 +0800

    修复

---
+++ src/com/server/controller/demo/Demo_query_loginController.java
@@ -0,0 +1,24 @@
+package com.server.controller.demo;
+
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.server.controller.BaseController;
+
+/**身份验证*/
+@RestController("demo_query_loginController")
+@RequestMapping("server/controller/demo_query_loginController")
+public class Demo_query_loginController extends BaseController{
+
+	/**根据账号获得密码*/
+	@RequestMapping(value="/loginConfirmation", method = RequestMethod.POST)
+    public String loginConfirmation(String  userName,HttpServletRequest request){
+		String password="123456";
+		return "{\"userName\":\""+userName+"\",\"password\":\""+password+"\"} ";
+    }
+
+}
