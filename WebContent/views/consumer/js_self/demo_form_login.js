78082d913469e32045710060c1be4206cf1f5216
nyszhangyong <nyszhangyong@foxmail.com>
2016-10-25 10:09:15 +0800

    修复

--- WebContent/views/consumer/js_self/demo_form_login.js
+++ WebContent/views/consumer/js_self/demo_form_login.js
@@ -2,13 +2,13 @@ $(function(){
    $("#loginbtn").on('click',function(){
 		$.ajax({ 
 			type: "post", 
-			url: "/UIPC/server/controller/demo_form_001_loginController/loginConfirmation", 
+			url: "/UIPC/server/controller/demo_form_loginController/loginConfirmation", 
 			data: $("#loginForm").serialize(),//serialize方式，表单中的DOM元素的属性必须要有name属性，仅有ID不行
 			success: function(returnMessage) {
-				//alert(returnMessage);
+				alert(returnMessage);
 				var obj = jQuery.parseJSON(returnMessage)
-				$("#userName").val(obj.userName);
-				$("#password").val(obj.password);
+				//$("#userName").val(obj.userName);
+				//$("#password").val(obj.password);
 				$("#login_result").text("登录成功！");
 			},
 			error: function(XMLHttpRequest, textStatus, errorThrown) {
