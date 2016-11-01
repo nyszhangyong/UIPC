06e9bd1e7d46f413da0c0a30bbb9d408950ccc02
nyszhangyong <nyszhangyong@foxmail.com>
2016-11-01 13:24:43 +0800

    修复

---
+++ srcHttpClient/com/server/controller/demo/Demo_form_loginController_HttpClient.java
@@ -0,0 +1,9 @@
+package com.server.controller.demo;
+
+public class Demo_form_loginController_HttpClient {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ srcHttpClient/com/server/controller/demo/HttpAccessDemo/Demo_HttpAccessServerController_HttpClient.java
@@ -0,0 +1,16 @@
+package com.server.controller.demo.HttpAccessDemo;
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import com.util.FilesUtils;
+import com.util.ObjectMessage;
+import com.vo.User;
+
+/**HTTP协议数据交互演示样例*/
+public class Demo_HttpAccessServerController_HttpClient{
+
+}
