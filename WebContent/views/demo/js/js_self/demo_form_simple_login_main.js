6b717908c1db0f88ded124edfc1e1176e2fe7605
nyszhangyong <nyszhangyong@foxmail.com>
2016-11-09 15:10:44 +0800

    修复

--- WebContent/views/demo/js/js_self/demo_form_simple_login_main.js
+++ WebContent/views/demo/js/js_self/demo_form_simple_login_main.js
@@ -2,39 +2,56 @@ $(function(){
 	var location = (window.location+'').split('/'); 
 	var basePath = location[0]+'//'+location[2]+'/'+location[3]; 
     $("#tabs").tabs();
-    $("#jsppage").load(basePath+"/views/demo/jsp/demo_form_login.jsp");
-    
-    var code_javascript = document.getElementById("code_javascript");
-    code_javascript.value ="66666666666";
-    window.editor = CodeMirror.fromTextArea(code_javascript, {
-      mode: "javascript",
-      lineNumbers: true,
-      lineWrapping: true,
-      extraKeys: {"Ctrl-Q": function(cm){ cm.foldCode(cm.getCursor()); }},
-      foldGutter: true,
-      gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"]
+    $("#jsppage").load(basePath+"/views/demo/jsp/demo_form_simple_login.jsp");
+    $.ajax({
+        url: basePath+"/views/demo/jsp/demo_form_simple_login.jsp",
+        type: "GET",
+        success: function(data){
+            $("#code_html").val(data);
+            var code_html = document.getElementById("code_html");
+            code_html.value = $("#code_html").val();
+            window.editor_html = CodeMirror.fromTextArea(code_html, {
+              mode: "text/html",
+              lineNumbers: true,
+              lineWrapping: true,
+              extraKeys: {"Ctrl-Q": function(cm){ cm.foldCode(cm.getCursor()); }},
+              foldGutter: true,
+              gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"]
+            });
+        }
     });
-
-    var code_html = document.getElementById("code_html");
-    code_html.value = document.documentElement.innerHTML;
-    window.editor_html = CodeMirror.fromTextArea(code_html, {
-      mode: "text/html",
-      lineNumbers: true,
-      lineWrapping: true,
-      extraKeys: {"Ctrl-Q": function(cm){ cm.foldCode(cm.getCursor()); }},
-      foldGutter: true,
-      gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"]
+    $.ajax({
+        url: basePath+"/views/demo/css/demo_form_simple_login.css",
+        type: "GET",
+        success: function(data){
+            $("#code_css").val(data);
+            var code_css = document.getElementById("code_css");
+            code_css.value = $("#code_css").val();
+            window.editor_markdown = CodeMirror.fromTextArea(code_css, {
+              mode: "markdown",
+              lineNumbers: true,
+              lineWrapping: true,
+              extraKeys: {"Ctrl-Q": function(cm){ cm.foldCode(cm.getCursor()); }},
+              foldGutter: true,
+              gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"]
+            });	  
+        }
     });
-
-
-    var code_css = document.getElementById("code_css");
-    code_css.value = "# Foo\n## Bar\n\nblah blah\n\n## Baz\n\nblah blah\n\n# Quux\n\nblah blah\n"
-    window.editor_markdown = CodeMirror.fromTextArea(code_css, {
-      mode: "markdown",
-      lineNumbers: true,
-      lineWrapping: true,
-      extraKeys: {"Ctrl-Q": function(cm){ cm.foldCode(cm.getCursor()); }},
-      foldGutter: true,
-      gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"]
-    });	  
+    $.ajax({
+        url: basePath+"/views/demo/js/js_self/demo_form_simple_login.js",
+        type: "GET",
+        success: function(data){
+            $("#code_javascript").val(data);
+            var code_javascript = document.getElementById("code_javascript");
+            code_javascript.value =$("#code_javascript").val();
+            window.editor = CodeMirror.fromTextArea(code_javascript, {
+              mode: "javascript",
+              lineNumbers: true,
+              lineWrapping: true,
+              extraKeys: {"Ctrl-Q": function(cm){ cm.foldCode(cm.getCursor()); }},
+              foldGutter: true,
+              gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"]
+            });
+        }
+    });
 });
