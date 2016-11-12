06cc9a22e701f94412047fffd16e024266bc8772
nyszhangyong <nyszhangyong@foxmail.com>
2016-11-12 17:13:07 +0800

    修复

--- WebContent/views/demo/js/js_self/demo_form_simple_picture.js
+++ WebContent/views/demo/js/js_self/demo_form_simple_picture.js
@@ -1,23 +1,23 @@
 $(function() {
   $("#pic").click(function () {
-    $("#upload").click();               //隐藏了input:file样式后，点击头像就可以本地上传
+     $("#upload").click();
      $("#upload").on("change",function(){
-       var objUrl = getObjectURL(this.files[0]) ;  //获取图片的路径，该路径不是图片在本地的路径
+       var objUrl = getObjectURL(this.files[0]) ;  
        if (objUrl) {
-         $("#pic").attr("src", objUrl) ;      //将图片路径存入src中，显示出图片
+         $("#pic").attr("src", objUrl) ;      
        }
     });
   });
 });
- 
-//建立一個可存取到該file的url
+
+//建立一个可存取FILE的URL
 function getObjectURL(file) {
   var url = null ;
-  if (window.createObjectURL!=undefined) { // basic
+  if (window.createObjectURL!=undefined) {//BASIC
     url = window.createObjectURL(file) ;
-  } else if (window.URL!=undefined) { // mozilla(firefox)
+  } else if (window.URL!=undefined) {//MOZILLA(FIREFOX)
     url = window.URL.createObjectURL(file) ;
-  } else if (window.webkitURL!=undefined) { // webkit or chrome
+  } else if (window.webkitURL!=undefined) {//WEBKIT OR CHROME
     url = window.webkitURL.createObjectURL(file) ;
   }
   return url ;
