6f4b0bb8d0ebba0dd2da96d5381592a8895d5b72
zhangyong <zhangyong@XB-20161129VPRV>
2016-12-03 16:39:28 +0800

    修复

--- WebContent/views/demo/js/js_self/demo_query_navigation_a_main.js
+++ WebContent/views/demo/js/js_self/demo_query_navigation_a_main.js
@@ -1,9 +1,11 @@
-﻿$("#nav .tit").slide({
-	type:"menu",
-	titCell:".mod_cate",
-	targetCell:".mod_subcate",
-	delayTime:0,
-	triggerTime:10,
-	defaultPlay:false,
-	returnDefault:true
+﻿$(function(){
+	$("#nav .tit").slide({
+		type:"menu",
+		titCell:".mod_cate",
+		targetCell:".mod_subcate",
+		delayTime:0,
+		triggerTime:10,
+		defaultPlay:false,
+		returnDefault:true
+	});
 });
